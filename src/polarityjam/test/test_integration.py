ed86067350a2acac6231f16e44a94837518b2d5d
Jan Philipp Albrecht <j.p.albrecht@fu-berlin.de>
2022-11-30 11:35:17 +0100

    fix feature count in tests

--- src/polarityjam/test/test_integration.py
+++ src/polarityjam/test/test_integration.py
@@ -49,7 +49,7 @@ class TestIntegration(TestCommon):
         self.assertAlmostEqual(df.shape[0], 97, delta=10)
 
         # number of features should not change
-        self.assertEqual(64, df.shape[1])
+        self.assertEqual(65, df.shape[1])
 
         # only one csv file in output
         num_csv = len(glob.glob(str(Path(out_path).joinpath("*.csv"))))
@@ -77,8 +77,8 @@ class TestIntegration(TestCommon):
         self.assertAlmostEqual(99, df2.shape[0], delta=10)
 
         # number of features should not change
-        self.assertEqual(64, df1.shape[1])
-        self.assertEqual(64, df2.shape[1])
+        self.assertEqual(65, df1.shape[1])
+        self.assertEqual(65, df2.shape[1])
 
         # two csv file in output
         num_csv = len(glob.glob(str(Path(out_path).joinpath("*.csv"))))
@@ -108,9 +108,9 @@ class TestIntegration(TestCommon):
         self.assertAlmostEqual(58, df3.shape[0], delta=7)
 
         # number of features should not change
-        self.assertEqual(59, df1.shape[1])
-        self.assertEqual(59, df2.shape[1])
-        self.assertEqual(59, df3.shape[1])
+        self.assertEqual(60, df1.shape[1])
+        self.assertEqual(60, df2.shape[1])
+        self.assertEqual(60, df3.shape[1])
         # three csv file in output
         num_csv = len(glob.glob(str(Path(out_path).joinpath("*.csv"))))
         self.assertEqual(3, num_csv)
@@ -141,9 +141,9 @@ class TestIntegration(TestCommon):
         self.assertAlmostEqual(29, df3.shape[0], delta=4)
 
         # number of features should not change
-        self.assertEqual(41, df1.shape[1])
-        self.assertEqual(41, df2.shape[1])
-        self.assertEqual(41, df3.shape[1])
+        self.assertEqual(42, df1.shape[1])
+        self.assertEqual(42, df2.shape[1])
+        self.assertEqual(42, df3.shape[1])
 
         # three csv file in output
         num_csv = len(glob.glob(str(Path(out_path).joinpath("*.csv"))))

212240c7b80068affd523daa625c12c0631423c7
Jan Philipp Albrecht <j.p.albrecht@fu-berlin.de>
2023-02-17 15:00:55 +0100

    fix import bug

--- src/polarityjam/__init__.py
+++ src/polarityjam/__init__.py
@@ -1,15 +1,11 @@
 __version__ = "0.1.1"
 __author__ = "Jan Philipp Albrecht, Wolfgang Giese"
 
+# imports for python API - do not delete!
+from polarityjam.model.parameter import RuntimeParameter, PlotParameter, SegmentationParameter, ImageParameter
+from polarityjam.model.collection import PropertiesCollection
+from polarityjam.model.masks import BioMedicalMask, BioMedicalInstanceSegmentationMask, BioMedicalInstanceSegmentation
+from polarityjam.model.image import BioMedicalImage
 from polarityjam.controller.extractor import Extractor
 from polarityjam.controller.plotter import Plotter
 from polarityjam.controller.segmenter import CellposeSegmenter
-from polarityjam.model.collection import PropertiesCollection
-from polarityjam.model.image import BioMedicalImage
-from polarityjam.model.masks import (BioMedicalInstanceSegmentation,
-                                     BioMedicalInstanceSegmentationMask,
-                                     BioMedicalMask)
-# imports for python API - do not delete!
-from polarityjam.model.parameter import (ImageParameter, PlotParameter,
-                                         RuntimeParameter,
-                                         SegmentationParameter)

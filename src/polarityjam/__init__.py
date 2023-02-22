987df8faaa5cd62a2e44daf11767cad61cf51d74
Jan Philipp Albrecht <j.p.albrecht@fu-berlin.de>
2023-02-22 11:08:47 +0100

    bump version 0.1.3

--- src/polarityjam/__init__.py
+++ src/polarityjam/__init__.py
@@ -1,10 +1,19 @@
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Jan Philipp Albrecht, Wolfgang Giese"
 
 # imports for python API - do not delete!
-from polarityjam.model.parameter import RuntimeParameter, PlotParameter, SegmentationParameter, ImageParameter
+from polarityjam.model.parameter import (
+    RuntimeParameter,
+    PlotParameter,
+    SegmentationParameter,
+    ImageParameter,
+)
 from polarityjam.model.collection import PropertiesCollection
-from polarityjam.model.masks import BioMedicalMask, BioMedicalInstanceSegmentationMask, BioMedicalInstanceSegmentation
+from polarityjam.model.masks import (
+    BioMedicalMask,
+    BioMedicalInstanceSegmentationMask,
+    BioMedicalInstanceSegmentation,
+)
 from polarityjam.model.image import BioMedicalImage
 from polarityjam.controller.extractor import Extractor
 from polarityjam.controller.plotter import Plotter

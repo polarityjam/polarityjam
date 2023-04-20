fix junction ratio calculation

src/polarityjam/controller/segmenter.py
@@ -1,5 +1,6 @@
 """Segmentation class base and CellposeSegmentation class."""
 from abc import ABCMeta, abstractmethod
+from enum import Enum
 from pydoc import locate
 from typing import Optional, Tuple
 
@@ -11,6 +12,14 @@ from polarityjam.model.parameter import (
     SegmentationAlgorithmE,
     SegmentationParameter,
 )
+
+
+class SegmentationMode(Enum):
+    """The segmentation mode."""
+
+    NUCLEUS = "nucleus"
+    CELL = "cell"
+    ORGANELLE = "organelle"
 
 
 class Segmenter:
@@ -25,7 +34,10 @@ class Segmenter:
 
     @abstractmethod
     def segment(
-        self, img: np.ndarray, path: Optional[str] = None, mode: Optional[str] = None
+        self,
+        img: np.ndarray,
+        path: Optional[str] = None,
+        mode: Optional[SegmentationMode] = SegmentationMode.CELL,
     ) -> np.ndarray:
         """Perform segmentation and return a mask image.
 

src/polarityjam/segmentation/cellpose.py
@@ -8,7 +8,7 @@ import skimage
 from skimage import morphology
 
 from polarityjam import ImageParameter
-from polarityjam.controller.segmenter import Segmenter
+from polarityjam.controller.segmenter import SegmentationMode, Segmenter
 from polarityjam.polarityjam_logging import get_logger
 
 
@@ -24,7 +24,7 @@ class CellposeSegmenter(Segmenter):
         self,
         img: np.ndarray,
         path: Optional[Union[Path, str]] = None,
-        mode: Optional[str] = None,
+        mode: Optional[SegmentationMode] = SegmentationMode.CELL,
     ) -> np.ndarray:
         """Perform the segmentation of the given image.
 
@@ -42,7 +42,7 @@ class CellposeSegmenter(Segmenter):
             A mask as np.ndarray image.
 
         """
-        cells = False if mode == "nucleus" else True
+        cells = False if mode == SegmentationMode.NUCLEUS else True
         get_logger().info(
             "Start segmentation procedure for %s..." % ("cells" if cells else "nuclei")
         )

src/polarityjam/segmentation/sam.py
@@ -7,7 +7,7 @@ from typing import Any, Dict, List, Optional, Tuple
 import numpy as np
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 
-from polarityjam.controller.segmenter import Segmenter
+from polarityjam.controller.segmenter import SegmentationMode, Segmenter
 from polarityjam.model.parameter import ImageParameter, SegmentationParameter
 from polarityjam.polarityjam_logging import get_logger
 from polarityjam.utils.url import download_resource
@@ -16,19 +16,23 @@ from polarityjam.utils.url import download_resource
 class SamSegmenter(Segmenter):
     """SAM segmentation model."""
 
-    MODEL_URL = "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth"
-    MODEL_URL_L = "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth"
     DOWNLOAD_PATH_REL = Path(os.path.dirname(os.path.realpath(__file__))).joinpath(
-        "SAM", "sam_vit_h_4b8939.pth"
+        "SAM"
     )
 
     def __init__(self, params: SegmentationParameter):
         """Initialize the segmenter with the given parameters."""
         self.params = params
-        self.model_path = None
+        self.model_url = params.model_url  # type: ignore
+        self.model_path = self.DOWNLOAD_PATH_REL.joinpath(
+            os.path.split(self.model_url)[-1]
+        )
 
     def segment(
-        self, img: np.ndarray, path: Optional[str] = None, mode: Optional[str] = None
+        self,
+        img: np.ndarray,
+        path: Optional[str] = None,
+        mode: Optional[SegmentationMode] = SegmentationMode.CELL,
     ) -> np.ndarray:
         """Segment the given image.
 
@@ -44,11 +48,11 @@ class SamSegmenter(Segmenter):
             The segmented image as numpy array.
 
         """
-        if mode == "nucleus":
+        if mode == SegmentationMode.NUCLEUS:
             return self._segment_single_channel(img[:, :, 1])
-        elif mode == "cell":
+        elif mode == SegmentationMode.CELL:
             return self._segment_single_channel(img[:, :, 0])
-        elif mode == "organelle":
+        elif mode == SegmentationMode.ORGANELLE:
             return self._segment_single_channel(img[:, :, 2])
         else:
             raise ValueError("Mode must be either nucleus, organelle or cell.")
@@ -91,8 +95,8 @@ class SamSegmenter(Segmenter):
             Tuple of the prepared image and the image parameter.
 
         """
-        if not self.DOWNLOAD_PATH_REL.exists():
-            download_resource(self.MODEL_URL, self.DOWNLOAD_PATH_REL)
+        if not self.model_path.exists():
+            download_resource(self.model_url, self.model_path)
 
         params_prep_img = ImageParameter()
         px_to_m_r = img_parameter.pixel_to_micron_ratio

src/polarityjam/test/integration/test_properties.py
@@ -92,7 +92,7 @@ class TestSingleCellProps(TestCommon):
         img_sc_dne = BioMedicalImage(self.sc_dne, self.img_param, segmentation=self.seg)
 
         sc_mask = img_sc_de_p.get_single_cell_mask(1)
-        sc_membrane_mask = img_sc_de_p.get_single_membrane_mask(1, 10)
+        sc_membrane_mask = img_sc_de_p.get_single_membrane_mask(1, 3)
         sc_junction_protein_mask = img_sc_de_p.get_single_junction_maks(1, 10)
 
         sc_mask_f = np.flip(sc_mask.data, axis=0)
@@ -154,7 +154,7 @@ class TestSingleCellProps(TestCommon):
         img_sc_une = BioMedicalImage(self.sc_une, self.img_param, segmentation=self.seg)
 
         sc_mask = img_sc_ue_p.get_single_cell_mask(1)
-        sc_membrane_mask = img_sc_ue_p.get_single_membrane_mask(1, 10)
+        sc_membrane_mask = img_sc_ue_p.get_single_membrane_mask(1, 3)
         sc_junction_protein_mask = img_sc_ue_p.get_single_junction_maks(1, 10)
 
         sc_mask_f = np.flip(sc_mask.data, axis=0)
@@ -218,7 +218,7 @@ class TestSingleCellProps(TestCommon):
         img_sc_dne = BioMedicalImage(self.sc_dne, self.img_param, segmentation=self.seg)
 
         sc_mask = img_sc_de_p.get_single_cell_mask(1)
-        sc_membrane_mask = img_sc_de_p.get_single_membrane_mask(1, 10)
+        sc_membrane_mask = img_sc_de_p.get_single_membrane_mask(1, 3)
 
         sc_mask_f = np.flip(sc_mask.data, axis=0)
         contour = get_contour(sc_mask_f.astype(int))
@@ -265,7 +265,7 @@ class TestSingleCellProps(TestCommon):
             junction_props_de_n.junction_cue_directional_intensity_ratio, -0.9
         )
         self.assertAlmostEqual(
-            junction_props_dne.junction_cue_directional_intensity_ratio, 0, delta=0.1
+            junction_props_dne.junction_cue_directional_intensity_ratio, 0, delta=0.14
         )
 
     def test_junction_property_cue_undirectional_intensity_ratio(self):
@@ -281,7 +281,7 @@ class TestSingleCellProps(TestCommon):
         img_sc_une = BioMedicalImage(self.sc_une, self.img_param, segmentation=self.seg)
 
         sc_mask = img_sc_ue_p.get_single_cell_mask(1)
-        sc_membrane_mask = img_sc_ue_p.get_single_membrane_mask(1, 10)
+        sc_membrane_mask = img_sc_ue_p.get_single_membrane_mask(1, 3)
 
         sc_mask_f = np.flip(sc_mask.data, axis=0)
         contour = get_contour(sc_mask_f.astype(int))
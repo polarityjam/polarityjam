changed parameter name plot_statistics to show_statistics, corrected inconsistancy in statistics.py regarding the units degrees and radians

src/polarityjam/compute/statistics.py
@@ -2,8 +2,9 @@
 
 
 import numpy as np
+from polarityjam.polarityjam_logging import get_logger
 
-def compute_polarity_index(angles: np.ndarray, cue_direction:  float = 0.0, stats_mode: str = 'directional'):
+def compute_polarity_index(angles: np.ndarray, cue_direction:  float = 0.0, stats_mode: str = 'directional', unit: str = 'radians'):
     """Compute the polarity index of a list of angles.
 
     Args:
@@ -13,9 +14,11 @@ def compute_polarity_index(angles: np.ndarray, cue_direction:  float = 0.0, stat
             direction of the imposed cue, used for V-score computation
         stats_mode:
             'directional' or 'axial'
+        unit:
+            'degrees' or 'radians'
 
     Returns:
-        list of values [alpha_m, R, c], alpha_m is the mean direction, R is the resultant vector length, c is a measure
+        list of values [alpha_m, R, c], alpha_m is the mean direction (in degrees), R is the resultant vector length, c is a measure
         of deviation from the mean angle, used to calculate the V-score: V=cR
 
     """
@@ -25,12 +28,15 @@ def compute_polarity_index(angles: np.ndarray, cue_direction:  float = 0.0, stat
     sum_cos = 0.0
     sum_sin = 0.0
 
-    if cue_direction > 360.0:
-        get_logger().warn("Warning invalid cue direction, must be between 0 and 360 degrees")
+    #if cue_direction > 360.0:
+    #    get_logger().warn("Warning invalid cue direction, must be between 0 and 360 degrees")
 
     p = 1.0
     if stats_mode == 'axial':
         p = 2.0
+
+    if unit == 'degrees':
+        angles = np.deg2rad(angles)
 
     for angle in angles:
         sum_cos += np.cos(p*angle)
@@ -41,11 +47,14 @@ def compute_polarity_index(angles: np.ndarray, cue_direction:  float = 0.0, stat
 
     R = np.sqrt(r_x**2 + r_y**2)
     alpha_m = np.arctan2(r_y, r_x)/p
-    cue_direction_ = np.deg2rad(cue_direction)
+
+    if unit == 'degrees':
+        cue_direction = np.deg2rad(cue_direction)
+
     if stats_mode == 'axial':
-        if cue_direction_ >= np.pi:
-            cue_direction_ -= np.pi
-    c = np.cos(alpha_m - cue_direction_)
+        if cue_direction >= np.pi:
+            cue_direction -= np.pi
+    c = np.cos(alpha_m - cue_direction)
 
     alpha_m = np.rad2deg(alpha_m)
 

src/polarityjam/controller/plotter.py
@@ -433,10 +433,12 @@ class Plotter:
                 )
 
         plot_title = "organelle polarity"
-        if self.params.plot_statistics:
+        if self.params.show_statistics:
             angles = np.array(collection.get_properties_by_img_name(img_name)["organelle_orientation_rad"])
-            alpha_m, R, c = compute_polarity_index(angles, cue_direction=cue_direction, stats_mode='directional')
-            plot_title += "\n mean angle: " + str(np.round(alpha_m, 2)) + "°, "
+            cue_direction_rad = np.deg2rad(cue_direction)
+            alpha_m, R, c = compute_polarity_index(angles, cue_direction=cue_direction_rad, stats_mode='directional')
+            plot_title += "\n N: " + str(len(angles)) + ", "
+            plot_title += "mean angle: " + str(np.round(alpha_m, 2)) + "°, "
             plot_title += "PI: " + str(np.round(R, 2)) + ","
             plot_title += "\n polarity cue: " + str(np.round(cue_direction, 2)) + "°, "
             plot_title += "c: " + str(np.round(c, 2)) + ", "
@@ -542,10 +544,12 @@ class Plotter:
                 )
 
         plot_title = "nucleus displacement orientation"
-        if self.params.plot_statistics:
+        if self.params.show_statistics:
             angles = np.array(collection.get_properties_by_img_name(img_name)["nuc_displacement_orientation_rad"])
-            alpha_m, R, c = compute_polarity_index(angles, cue_direction=cue_direction, stats_mode='directional')
-            plot_title += "\n mean angle: " + str(np.round(alpha_m, 2)) + "°, "
+            cue_direction_rad = np.deg2rad(cue_direction)
+            alpha_m, R, c = compute_polarity_index(angles, cue_direction=cue_direction_rad, stats_mode='directional')
+            plot_title += "\n N: " + str(len(angles)) + ", "
+            plot_title += "mean angle: " + str(np.round(alpha_m, 2)) + "°, "
             plot_title += "PI: " + str(np.round(R, 2)) + ","
             plot_title += "\n polarity cue: " + str(np.round(cue_direction, 2)) + "°, "
             plot_title += "c: " + str(np.round(c, 2)) + ", "
@@ -737,6 +741,7 @@ class Plotter:
 
         pixel_to_micron_ratio = img.img_params.pixel_to_micron_ratio
         r_params = collection.get_runtime_params_by_img_name(img_name)
+        cue_direction = r_params.cue_direction
 
         get_logger().info("Plotting: marker polarity")
 
@@ -771,11 +776,13 @@ class Plotter:
             )
 
         plot_title = "marker polarity"
-        if self.params.plot_statistics:
+        if self.params.show_statistics:
             angles = np.array(collection.get_properties_by_img_name(img_name)["marker_centroid_orientation_rad"])
-            alpha_m, R, c = compute_polarity_index(angles, cue_direction=r_params.cue_direction, stats_mode='directional')
+            cue_direction_rad = np.deg2rad(cue_direction)
+            alpha_m, R, c = compute_polarity_index(angles, cue_direction=cue_direction_rad, stats_mode='directional')
             #plot_title += "\n mean \u03B1: " + str(np.round(alpha_m, 2)) + "°, "
-            plot_title += "\n mean angle: " + str(np.round(alpha_m, 2)) + "°, "
+            plot_title += "\n N: " + str(len(angles)) + ", "
+            plot_title += "mean angle: " + str(np.round(alpha_m, 2)) + "°, "
             plot_title += "PI: " + str(np.round(R, 2)) + ","
             plot_title += "\n polarity cue: " + str(np.round(alpha_m, 2)) + "°, "
             plot_title += "c: " + str(np.round(c, 2)) + ", "
@@ -887,11 +894,13 @@ class Plotter:
                 )
 
         plot_title = "marker nucleus orientation"
-        if self.params.plot_statistics:
+        if self.params.show_statistics:
             angles = np.array(collection.get_properties_by_img_name(img_name)["marker_nucleus_orientation_rad"])
-            alpha_m, R, c = compute_polarity_index(angles, cue_direction=cue_direction, stats_mode='directional')
+            cue_direction_rad = np.deg2rad(cue_direction)
+            alpha_m, R, c = compute_polarity_index(angles, cue_direction=cue_direction_rad, stats_mode='directional')
             #plot_title += "\n mean \u03B1: " + str(np.round(alpha_m, 2)) + "°, "
-            plot_title += "\n mean angle: " + str(np.round(alpha_m, 2)) + "°, "
+            plot_title += "\n N: " + str(len(angles)) + ", "
+            plot_title += "mean angle: " + str(np.round(alpha_m, 2)) + "°, "
             plot_title += "PI: " + str(np.round(R, 2)) + ","
             plot_title += "\n polarity cue: " + str(np.round(cue_direction, 2)) + "°, "
             plot_title += "c: " + str(np.round(c, 2)) + ", "
@@ -966,7 +975,7 @@ class Plotter:
             )
 
         plot_title = "junction polarity"
-        #if self.params.plot_statistics:
+        #if self.params.show_statistics:
         #    angles = np.array(collection.get_properties_by_img_name(img_name)["junction_polarity_rad"])
         #    alpha_m, R, c = compute_polarity_index(angles, cue_direction=r_params.cue_direction, stats_mode='directional')
         #    plot_title += "\n mean \u03B1: " + str(np.round(alpha_m, 2)) + "°, "
@@ -1891,10 +1900,12 @@ class Plotter:
                 )
 
         plot_title = "cell shape orientation"
-        if self.params.plot_statistics:
+        if self.params.show_statistics:
             angles = np.array(collection.get_properties_by_img_name(img_name)["cell_shape_orientation_rad"])
-            alpha_m, R, c = compute_polarity_index(angles, cue_direction=cue_direction, stats_mode='axial')
-            plot_title += "\n mean angle: " + str(np.round(alpha_m, 2)) + "°, "
+            cue_direction_rad = np.deg2rad(cue_direction)
+            alpha_m, R, c = compute_polarity_index(angles, cue_direction = cue_direction_rad, stats_mode='axial')
+            plot_title += "\n N: " + str(len(angles)) + ", "
+            plot_title += "mean angle: " + str(np.round(alpha_m, 2)) + "°, "
             plot_title += "PI: " + str(np.round(R, 2)) + ","
             plot_title += "\n polarity cue: " + str(np.round(cue_direction, 2)) + "°, "
             plot_title += "c: " + str(np.round(c, 2)) + ", "
@@ -1909,10 +1920,12 @@ class Plotter:
                 self.params.show_graphics_axis,
             )
             plot_title_nuc = "nuclei shape orientation"
-            if self.params.plot_statistics:
+            if self.params.show_statistics:
                 angles = np.array(collection.get_properties_by_img_name(img_name)["nuc_shape_orientation_rad"])
-                alpha_m, R, c = compute_polarity_index(angles, cue_direction=cue_direction, stats_mode='axial')
-                plot_title_nuc += "\n mean angle: " + str(np.round(alpha_m, 2)) + "°, "
+                cue_direction_rad = np.deg2rad(cue_direction)
+                alpha_m, R, c = compute_polarity_index(angles, cue_direction=cue_direction_rad, stats_mode='axial')
+                plot_title += "\n N: " + str(len(angles)) + ", "
+                plot_title += "mean angle: " + str(np.round(alpha_m, 2)) + "°, "
                 plot_title_nuc += "PI: " + str(np.round(R, 2)) + ","
                 plot_title_nuc += "\n polarity cue: " + str(np.round(c, 2)) + "°, "
                 plot_title_nuc += "c: " + str(np.round(c, 2)) + ", "
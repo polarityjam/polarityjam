f580b62e9d84d63c8744a256739666f56d4f1503
Jan Philipp Albrecht <j.p.albrecht@fu-berlin.de>
2025-10-01 15:51:52 +0200

    update dependencies

--- album_solution/solution.py
+++ album_solution/solution.py
@@ -1,5 +1,4 @@
 """polarityjam album solution."""
-import pkg_resources
 from album.runner.api import get_args, setup
 
 env_file = """name: Polarityjam
@@ -9,23 +8,8 @@ dependencies:
   - python~3.10
   - pip
   - pip:
-     - polarityjam==0.4.0
+     - polarityjam==0.4.1
 """
-
-album_version = None
-installed_packages = pkg_resources.working_set
-for package in installed_packages:
-    if package.key == "album":
-        album_version = package.version
-        break
-
-if album_version is not None:
-    if str(album_version) != "0.10.3":
-        if str(album_version) != "0.10.4":
-            raise RuntimeError(
-                "This solution requires album version 0.10.3 or 0.10.4, found %s"
-                % album_version
-            )
 
 
 def run():
